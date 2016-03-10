e85456268863b9dd076eb04bcc739570dc90cca8
WJM <wjmwjmwb@yeah.net>
2016-03-10 16:54:19 +0800

    Initial Commit

---
+++ HorizontalCollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalCollectionViewDemo
+//
+//  Created by WJM on 16/3/10.
+//  Copyright © 2016年 WJM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorizontalCollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorizontalCollectionViewDemo
+//
+//  Created by WJM on 16/3/10.
+//  Copyright © 2016年 WJM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
